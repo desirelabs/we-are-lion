92ce2bd3a764dc3f4a4bfd0b035ae7e658bd1880
Franck LEBAS <francklebas@protonmail.ch>
2020-05-22 15:54:43 +0200

    [RELEASE] 1.0.1 (#12)
    * build(deps): bump acorn from 6.4.0 to 6.4.1
    
    Bumps [acorn](https://github.com/acornjs/acorn) from 6.4.0 to 6.4.1.
    - [Release notes](https://github.com/acornjs/acorn/releases)
    - [Commits](https://github.com/acornjs/acorn/compare/6.4.0...6.4.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Feature/navigation menu (#8)
    
    * fix: render only if window is defined
    
    * refactor: move assets
    
    * refactor: cleanup code smells
    
    * feat: implement mobile menu with animation
    
    * feat: make homepage responsive (#9)
    
    * feat: implement contct page (#10)
    
    * fix: update grid
    
    * fix: fix link
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/components/ui/MenuWrapper.tsx
+++ src/components/ui/MenuWrapper.tsx
@@ -1,5 +1,5 @@
 import styled, { css } from "styled-components";
-import { Link } from "../Menu";
+import { MenuLink } from "../Menu";
 
 export const NavItem = styled.span`
   padding: 0 9px;
@@ -21,18 +21,13 @@ export const NavItem = styled.span`
 const mobileMenuOpened = css`
   width: 800px;
   height: 1200px;
-  //opacity: 1;
-  //transition: opacity 1s ease-in-out;
 `;
 
 const mobileMenuClosed = css`
   width: 5px;
   height: 5px;
   border-radius: 50%;
-  //opacity: 0;
-  //transition: opacity 1s ease-in-out;
-  //transition-delay: 1s;
-  ${Link} {
+  a {
     display: none;
   }
 `;
@@ -50,6 +45,9 @@ export const MenuWrapper = styled.nav<{
   max-width: ${({ theme }) => theme.atoms.layout.desktop.containerWidth};
   position: sticky;
   background-color: transparent;
+  a {
+    text-decoration: none;
+  }
   @media (max-width: 767px) {
     position: fixed;
     top: 0;
@@ -62,12 +60,15 @@ export const MenuWrapper = styled.nav<{
     align-items: center;
     max-width: 100%;
     ${({ toggle }) => (toggle ? mobileMenuOpened : mobileMenuClosed)}
-    ${NavItem} {
-      font-weight: 700;
-      font-size: 18px;
-      color: ${({ theme }) => theme.atoms.colors.white};
-      &:hover {
-        color: ${({ theme }) => theme.atoms.colors.grey};
+    a {
+      padding: 30px;
+      span {
+        font-weight: 700;
+        font-size: 18px;
+        color: ${({ theme }) => theme.atoms.colors.white};
+        &:hover {
+          color: ${({ theme }) => theme.atoms.colors.grey};
+        }
       }
     }
   }

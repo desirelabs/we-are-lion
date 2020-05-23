3b7197abf1c95aec16b2d039b2cd53888c744c07
Franck LEBAS <francklebas@protonmail.ch>
2020-05-23 06:29:36 +0200

    fix: remove svg animation

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MenuWrapper, NavItem } from "./ui/MenuWrapper";
 import { Link } from "gatsby";
 import styled, { css } from "styled-components";
-import { useEffect, useReducer } from "react";
 import { Icon } from "./ui/Icon";
-import { Animate } from "react-move";
-import { Surface } from "./ui-utils/Surface";
-import { interpolate } from "flubber";
-import { easeQuadOut } from "d3-ease";
 
 
 export const MenuLink = styled.span`
@@ -26,7 +21,6 @@ const MenuToggleOpenIcon = styled(Icon).attrs({
 })``;
 
 const TRANSITION_DELAY = 0.15;
-const ANIMATION_DURATION = 300;
 
 const visibleIcon = css`
   opacity: 1;
@@ -63,43 +57,8 @@ const MenuToggle = styled.a.attrs({
   }
 `;
 
-const SvgWrapper = styled.div`
-  position: fixed;
-  z-index: -1;
-  @media (min-width: 768px) {
-    display: none;
-  }
-`;
-
-const startPath =
-  "M413.5,0 C413.180328,0 413,0.297449832 413,0.5 C413,0.702550168 413.167386,1 413.5,1 C413.832614,1 414,0.700958882 414,0.5 C414,0.299041118 413.819672,0 413.5,0 Z";
-const endPath =
-  "M207,-51 C-112.032787,-51 -292,245.854932 -292,448 C-292,650.145068 -124.948653,947 207,947 C538.948653,947 706,648.556964 706,448 C706,247.443036 526.032787,-51 207,-51 Z";
-
-const initialState = {
-  toggle: false,
-  path: [endPath, startPath]
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "open":
-      return {
-        toggle: true,
-        path: [startPath, endPath]
-      };
-    case "close":
-      return {
-        toggle: false,
-        path: [endPath, startPath]
-      };
-    default:
-      return initialState;
-  }
-}
-
 export const Menu = ({ isClear }: { isClear?: boolean }) => {
-  const [{ toggle, path }, dispatch] = useReducer(reducer, initialState);
+  const [toggle, setToggle] = useState(false);
 
   /**
    * Escape keyboard press
@@ -107,7 +66,7 @@ export const Menu = ({ isClear }: { isClear?: boolean }) => {
    */
   function handleEscape(e) {
     if (e.code === "Escape" && toggle) {
-      dispatch({ type: "close" });
+      setToggle(false);
     }
   }
 
@@ -117,7 +76,7 @@ export const Menu = ({ isClear }: { isClear?: boolean }) => {
    */
   function handleToggle(e) {
     if (e.keyCode === 32 || e.keyCode === 13 || e.type === "click") {
-      toggle ? dispatch({ type: "close" }) : dispatch({ type: "open" });
+      toggle ? setToggle(false) : setToggle(true);
     }
   }
 
@@ -127,11 +86,6 @@ export const Menu = ({ isClear }: { isClear?: boolean }) => {
       return () => document.removeEventListener("keyup", e => handleEscape(e));
     }
   }, [handleEscape]);
-
-  /**
-   * Svg path interpolation
-   */
-  const interpolator = interpolate(path[0], path[1]);
 
   return (
     <>
@@ -143,7 +97,7 @@ export const Menu = ({ isClear }: { isClear?: boolean }) => {
         <MenuToggleCloseIcon />
         <MenuToggleOpenIcon />
       </MenuToggle>
-      <MenuWrapper isClear={isClear} toggle={toggle}>
+      <MenuWrapper isClear={isClear} toggle={toggle} className="menu-wrapper">
         <Link to="/" onClick={e => handleToggle(e)}>
           <NavItem>Accueil</NavItem>
         </Link>
@@ -151,32 +105,6 @@ export const Menu = ({ isClear }: { isClear?: boolean }) => {
           <NavItem>Contact</NavItem>
         </Link>
       </MenuWrapper>
-      <SvgWrapper>
-        <Surface view={[415, 1200]} trbl={[0, 0, 0, 0]}>
-          <Animate
-            start={{
-              opacity: 0,
-              d: interpolator(1)
-            }}
-            enter={[
-              {
-                opacity: [1],
-                timing: { duration: ANIMATION_DURATION }
-              }
-            ]}
-            update={{
-              d: interpolator,
-              timing: { duration: ANIMATION_DURATION, ease: easeQuadOut }
-            }}
-          >
-            {state => (
-              <g>
-                <path fill="#ff2e64" {...state} />
-              </g>
-            )}
-          </Animate>
-        </Surface>
-      </SvgWrapper>
     </>
   );
 };

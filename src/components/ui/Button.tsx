25a0f5b1330b7c13f5f20e697a6cb6d8a01fffbc
LEBAS <francklebas@protonmail.ch>
2020-05-27 15:47:28 +0200

    refactor: update button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,25 +1,55 @@
-import styled from "styled-components";
-import { Button as MaterialButton } from "@material-ui/core";
+import styled, { css } from "styled-components";
 
-export const Button = styled.button<{ isFull?: boolean }>`
-  text-transform: uppercase;
-  width: ${({ isFull }) => (isFull ? "100%" : "260px")};
+const large = css`
+  width: 100%;
   height: 48px;
   border-radius: 24px;
-  background-image: linear-gradient(
-      to top,
-      ${({ theme }) => theme && theme.atoms.colors.secondary},
-      ${({ theme }) => theme && theme.atoms.colors.primary}
-    ),
-    linear-gradient(to bottom, #c4c4c4, #c4c4c4);
-  font-family: Montserrat, sans-serif;
   font-size: 16px;
+`;
+
+const small = css`
+  width: 180px;
+  height: 36px;
+  border-radius: 18px;
+  font-size: 12px;
+`;
+
+const primary = css`
+  background-color: ${({ theme }) => theme && theme.atoms.colors.primary};
+  border: ${({ theme }) => theme && `2px solid ${theme.atoms.colors.primary}`};
+  color: ${({ theme }) => theme && theme.atoms.colors.white};
+`;
+
+const secondary = css`
+  background-color: ${({ theme }) => theme && theme.atoms.colors.white};
+  border: ${({ theme }) => theme && `2px solid ${theme.atoms.colors.primary}`};
+  color: ${({ theme }) => theme && theme.atoms.colors.dark};
+`;
+
+export const Button = styled.button<{
+  size?: "small" | "large";
+  variant?: "primary" | "secondary";
+}>`
+  width: 260px;
+  height: 36px;
+  border-radius: 18px;
+  font-size: 12px;
+  ${({ size }) => size && size === "large" && large};
+  ${({ size }) => size && size === "small" && small};
+
+  background-color: ${({ theme }) => theme && theme.atoms.colors.primary};
+  border: ${({ theme }) => theme && `2px solid ${theme.atoms.colors.primary}`};
+  color: ${({ theme }) => theme && theme.atoms.colors.white};
+  ${({ variant }) => variant && variant === "primary" && primary};
+  ${({ variant }) => variant && variant === "secondary" && secondary};
+
+  text-transform: uppercase;
+  font-family: Montserrat, sans-serif;
   font-weight: 500;
   font-stretch: normal;
   font-style: normal;
   line-height: 1;
   letter-spacing: normal;
-  color: ${({ theme }) => theme && theme.atoms.colors.white};
   cursor: pointer;
   box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
